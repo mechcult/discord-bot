d7cd4581a4fcc5b87b8252aca386c4a3e0b7ca2e
Sergey Redko <sergey@redko.me>
2020-04-01 14:47:01 +0300

    init

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # discord-bot
+
+## .env file:
+```js
+TOKEN=%your bot token%
+CHANNEL=%channel id to watch%
+ROLE=%role user has to have to vote%
+EMOJI=%emoji displaying vote, i.e. ➕%
+COUNT=%required count of votes to be promoted%
+```
---
+++ index.js
@@ -0,0 +1,49 @@
+require('dotenv').config()
+var dateFormat = require('dateformat');
+const Discord = require("discord.js")
+const client = new Discord.Client();
+const config = {
+    token: process.env.TOKEN,
+    channel: process.env.CHANNEL,
+    role: process.env.ROLE,
+    emoji: process.env.EMOJI,
+    count: parseInt(process.env.COUNT)
+};
+var d = new Date();
+const time = (d) => dateFormat(d, "yyyy.mm.dd, hh:MM:ss TT")
+
+
+client.login(config.token);
+
+client.on('ready', () => {
+    console.log(time(d) + `\tLogged in as ${client.user.tag}`);
+})
+
+client.on('message', message => {
+    if (message.channel.id !== config.channel) { return; }
+    else {
+        message.react(config.emoji)
+        console.log(time(d) + '\tGot message in channel and reacted')
+    }
+})
+
+client.on("messageReactionAdd", (react, user) => {
+    let member = react.message.guild.members.fetch(user.id)
+
+    if ((react.message.channel.id !== config.channel) ||
+        (react.emoji.name !== config.emoji) || 
+        (user.id === client.user.id)) {
+        return;
+    } else {console.log(time(d) + `\tGot reaction from ${user.tag}`);}
+
+    if (member.then(m => m.roles.cache.has(config.role))) {
+        if (react.count >= config.count + 1) {
+            console.log(time(d) + `\tUser ${react.message.author.tag} was promoted`)
+            react.message.member.roles.add('633669166058176512')
+        }
+        return;
+    } else {
+        console.log(time(d) + `\tUser ${user.tag} has no required role`)
+        react.users.remove(user);
+    }
+})
